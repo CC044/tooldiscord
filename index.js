7238186dc972a7e17e83e6fbc87b2c1e216a1d96
chris <cctitan53@gmail.com>
2021-08-13 02:18:53 +0200

    clean index

--- index.js
+++ index.js
@@ -3,27 +3,21 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login('ODc1MjUyMDMxOTk5MzIwMDg0.YRSz-Q.cCd9NKqxENQ88kdpvkRnSS6Rc4g');
+client.login(process.env.TOKEN);
+
+const channel = client.channels.cache.get('609902022052806688');
+console.log(`channel: ${channel}`);
+//client.login('ODc1MjUyMDMxOTk5MzIwMDg0.YRSz-Q.cCd9NKqxENQ88kdpvkRnSS6Rc4g');
 /*
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
 
-client.login(process.env.TOKEN);
 
-const channel = client.channels.cache.get('609902022052806688');
-console.log(`channel: ${channel}`)
 // channel.send('content');
 */
